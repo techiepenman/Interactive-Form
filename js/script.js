23e407228bcb34f0b78cee39f718937760558ac0
Mehdi Hajidavalloo <mehdihd@iSoul-MBP.home>
2021-01-30 19:08:47 -0800

    job field display and hide created, eventlistener for job menu is added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert('hello');
+const nameField = document.querySelector('#name').focus();
+const otherJobField = document.querySelector('#other-job-role');
+otherJobField.style.display = 'none';
+
+const jobTitle = document.querySelector('#title');
+
+jobTitle.addEventListener('change', (e) => {
+    const selectedJob = e.target.value;
+    if (selectedJob === 'other') {
+        otherJobField.style.display = '';
+    } else {
+        otherJobField.style.display = 'none'
+    }
+    
+});
+// console.log(jobTitle);
