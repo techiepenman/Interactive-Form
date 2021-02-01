4f067feb62ceebe2c6857be0fbf7499c91d4311c
Mehdi Hajidavalloo <mehdihd@iSoul-MBP.home>
2021-01-31 20:22:03 -0800

    Complete T-Shirt info section, color field is disabled first, upon design selection the colors will be activated corresponding to the selected theme

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const nameField = document.querySelector('#name').focus();
 const otherJobField = document.querySelector('#other-job-role');
-otherJobField.style.display = 'none';
-
+const colorSelect = document.querySelector('#color');
 const jobTitle = document.querySelector('#title');
+const shirtDesign = document.querySelector('#design');
 
+otherJobField.style.display = 'none';
+colorSelect.disabled = true;
 jobTitle.addEventListener('change', (e) => {
     const selectedJob = e.target.value;
     if (selectedJob === 'other') {
@@ -13,4 +15,26 @@ jobTitle.addEventListener('change', (e) => {
     }
     
 });
-// console.log(jobTitle);
+
+shirtDesign.addEventListener('change', (e) => {
+     
+        colorSelect.disabled = false;
+        for (let i = 1; i < colorSelect.children.length; i++) {
+            let colorTheme = colorSelect[i].getAttribute('data-theme');
+            let designTheme = e.target.value;
+        //    console.log(colorSelect[i].getAttribute('data-theme'));
+           console.log(colorTheme);
+           console.log(designTheme);
+           if (designTheme === colorTheme) {
+               colorSelect[i].hidden = false;
+           } else {
+            colorSelect[i].hidden = true;
+           }
+
+        }
+        
+  
+    // console.log(selectDesign);
+});
+
+// 
