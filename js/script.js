943c882a8b23f0e7a3022577b682aadc6f4255ab
Mehdi Hajidavalloo <mehdihd@iSoul-MBP.home>
2021-02-07 15:26:59 -0800

    Removed logs, added indentations

--- js/script.js
+++ js/script.js
@@ -27,27 +27,27 @@ jobTitle.addEventListener('change', (e) => {
     } else {
         otherJobField.style.display = 'none'
     }
-    
+
 });
 
 // Activate colors in color drop down based on the selected design from design drop down
 shirtDesign.addEventListener('change', (e) => {
-     e.preventDefault();
-        colorSelect.disabled = false;
-        for (let i = 1; i < colorSelect.children.length; i++) {
-            let colorTheme = colorSelect[i].getAttribute('data-theme');
-            let designTheme = e.target.value;
-                  
-           if (designTheme === colorTheme) {
-               colorSelect[i].hidden = false;
-               colorSelect[i].selected = true;
-           } else {
+    e.preventDefault();
+    colorSelect.disabled = false;
+    for (let i = 1; i < colorSelect.children.length; i++) {
+        let colorTheme = colorSelect[i].getAttribute('data-theme');
+        let designTheme = e.target.value;
+
+        if (designTheme === colorTheme) {
+            colorSelect[i].hidden = false;
+            colorSelect[i].selected = true;
+        } else {
             colorSelect[i].hidden = true;
             colorSelect[i].selected = false;
-           }
-
         }
-       
+
+    }
+
 });
 
 // Activity selection and pricing. Add to or deduct from the total cost by 
@@ -55,16 +55,16 @@ shirtDesign.addEventListener('change', (e) => {
 let printTotal = document.querySelector('#activities-cost');
 let total = 0;
 activities.addEventListener('change', (e) => {
- 
+
     if (e.target.checked) {
         let activityCost = e.target.getAttribute('data-cost');
         let costNumeric = parseInt(activityCost);
         total += costNumeric;
-        
+
     } else if (!e.target.checked) {
         let activityCost = e.target.getAttribute('data-cost');
         let costNumeric = parseInt(activityCost);
-        total -= costNumeric; 
+        total -= costNumeric;
     }
 
     printTotal.innerHTML = `Total: $${total}`;
@@ -72,13 +72,11 @@ activities.addEventListener('change', (e) => {
 
 // Add focus indicator to the activities check boxes so pressing tab key
 //will move the focus to make the activity more obvious 
-for (let i =0; i < activity.length; i++) {
-   activity[i].addEventListener('focus', e  => {
-        console.log(activity[i]);
+for (let i = 0; i < activity.length; i++) {
+    activity[i].addEventListener('focus', e => {
         activity[i].parentElement.classList.add('focus');
     });
-    activity[i].addEventListener('blur', e  => {
-        console.log(activity[i]);
+    activity[i].addEventListener('blur', e => {
         activity[i].parentElement.classList.remove('focus');
     });
 };
@@ -94,122 +92,114 @@ paymentOptions.children[1].setAttribute('selected', 'selected');
 paymentOptions.addEventListener('change', (e) => {
     let selectedPayment = e.target.value;
     switch (selectedPayment) {
-        case 'paypal' :
-            payPal.hidden = false;    
+        case 'paypal':
+            payPal.hidden = false;
             creditCard.hidden = true;
             bitCoin.hidden = true;
             break;
-        case 'bitcoin' :
+        case 'bitcoin':
             bitCoin.hidden = false;
             payPal.hidden = true;
             creditCard.hidden = true;
             break;
-        case 'credit-card' :
+        case 'credit-card':
             creditCard.hidden = false;
             bitCoin.hidden = true;
-            payPal.hidden = true;  
+            payPal.hidden = true;
             break;
-            default:
-                console.log('The option is not exist');
-    
-        }
+        default:
+            console.log('The option is not exist');
 
-    });
+    }
+
+});
 
 // Helper functions will be used to check the pattern 
 //of the user input in required fields
-    const nameValidator = (name) => {
-        const checkName = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(name.value);
-        return checkName;
-    };
+const nameValidator = (name) => {
+    const checkName = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(name.value);
+    return checkName;
+};
 
-    const emailValidator = (userEmail) => {
-        const checkEmail = /^[^@]+@[^@.]+\.[a-z]+$/i.test(userEmail.value);
-        return checkEmail;
-    };
-    const creditValidator = (card) => {
-        const checkCredit = /^\d{13,16}$/i.test(card.value);
-        return checkCredit;
-    };
+const emailValidator = (userEmail) => {
+    const checkEmail = /^[^@]+@[^@.]+\.[a-z]+$/i.test(userEmail.value);
+    return checkEmail;
+};
+const creditValidator = (card) => {
+    const checkCredit = /^\d{13,16}$/i.test(card.value);
+    return checkCredit;
+};
 
-    const zipValidator = (zipCode) => {
-        const checkZip = /^\d{5}$/i.test(zipCode.value);
-        return checkZip;
-    };
+const zipValidator = (zipCode) => {
+    const checkZip = /^\d{5}$/i.test(zipCode.value);
+    return checkZip;
+};
 
-    const cvvValidator = (cvvNum) => {
-        const cvvCheck = /^\d{3}$/i.test(cvvNum.value);
-        return cvvCheck;
-    };
+const cvvValidator = (cvvNum) => {
+    const cvvCheck = /^\d{3}$/i.test(cvvNum.value);
+    return cvvCheck;
+};
 
 // Helper functions for handling input error messages by
 //add or removal of designated classes
-  function error (element) {
-      element.parentElement.classList.add('not-valid');
-      element.parentElement.classList.remove('valid');
-      element.parentElement.lastElementChild.style.display = 'block';
-  };
+function error(element) {
+    element.parentElement.classList.add('not-valid');
+    element.parentElement.classList.remove('valid');
+    element.parentElement.lastElementChild.style.display = 'block';
+};
 
-  function noError (element) {
+function noError(element) {
     element.parentElement.classList.remove('not-valid');
     element.parentElement.classList.add('valid');
     element.parentElement.lastElementChild.style.display = 'none';
-  }
+}
 
- // Handle submit   
+// Handle submit   
 submitBtn.addEventListener('click', (e) => {
-     
+
     if (!nameValidator(nameField)) {
         e.preventDefault();
-        console.log('name is not valid');
         error(nameField);
     } else {
         noError(nameField);
     }
     if (!emailValidator(email)) {
         e.preventDefault();
-        console.log('email is not valid');
         error(email);
     } else {
         noError(email);
     }
-    
+
     if (total === 0) {
         e.preventDefault();
-        console.log('activity is not selected'); 
         error(activities.firstElementChild);
     } else {
         noError(activities.firstElementChild);
-    };    
-   
-   
+    };
+
+
     //Only validate credit card credentials if selected payment method 
     //is credit card
     if (paymentOptions.value === 'credit-card') {
-        console.log('credit is selected');
-       if (!creditValidator(cardNumber)) {
-          e.preventDefault();
-          console.log('credit card is not valid');
-          error(cardNumber);
-       } else {
-          noError(cardNumber);
-       }
-       if (!zipValidator(zip)) {
-          e.preventDefault();
-          console.log('zip is not valid');
-          error(zip);
+        if (!creditValidator(cardNumber)) {
+            e.preventDefault();
+            error(cardNumber);
+        } else {
+            noError(cardNumber);
+        }
+        if (!zipValidator(zip)) {
+            e.preventDefault();
+            error(zip);
         } else {
             noError(zip);
         }
-       if (!cvvValidator(cvv)) {
-        e.preventDefault();
-        console.log('cvv code is not valid');
-        error(cvv);
+        if (!cvvValidator(cvv)) {
+            e.preventDefault();
+            error(cvv);
         } else {
             noError(cvv);
         }
 
 
-}
+    }
 });
-
